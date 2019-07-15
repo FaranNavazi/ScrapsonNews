3279fc0c46a190cf6320150dbf805a5d32d13772
Faran Navazi <faran.navazi@gmail.com>
2019-07-15 18:48:41 -0500

    HW fully works and ready to be deployed

--- server.js
+++ server.js
@@ -1,86 +1,33 @@
-// Dependencies
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-// Require axios and cheerio. This makes the scraping possible
-var axios = require("axios");
-var cheerio = require("cheerio");
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const request = require("request");
+const cheerio = require("cheerio");
+const bodyParser = require('body-parser');
 
-// Require all models
-var db = require("./models");
 
-// Initialize Express
-var app = express();
+var PORT = process.env.PORT || 3030;
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
+const app = express();
+
+
 app.use(express.static("public"));
 
-// Hook mongoose configuration to the db variable
-var DB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapsonnews";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
 
-mongoose.connect(DB_URI, {
-  useNewUrlParser: true
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
 
-// Retrieve data from the db
-app.get("/json", function(req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.Article.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ScrapsonNews";
+
+// mongoose.connect(MONGODB_URI);
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
   });
-});
-
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-  // Make a request via axios for the news section of `ycombinator`
-  axios
-    .get("https://www.makeuseof.com/service/programming/")
-    .then(function(response) {
-      // Load the html body from axios into cheerio
-      var $ = cheerio.load(response.data);
-      // For each element with a "h1" tag
-      $("h1").each(function(i, element) {
-        var result = {};
-
-        result.title = $(this).text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-      });
-
-      // Send a "Scrape Complete" message to the browser
-      res.send("Scrape Complete");
-    });
-});
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
